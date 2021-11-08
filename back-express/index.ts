978a23688ca2e0a3a238bd414f5e9ce272a386ff
Bluub <louis.auzuret@gmail.com>
2021-11-08 17:14:20 +0100

    added swagger to express

--- back-express/index.ts
+++ back-express/index.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from "express";
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const app: Application = express();
 const port = 3000;
 
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get(
     "/",
