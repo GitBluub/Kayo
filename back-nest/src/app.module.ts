ac7e9bcbc8a4d6d8de5438de0075b07dafb12ee2
arthur.jamet <arthur.jamet@epitech.eu>
2021-11-10 16:13:19 +0100

    integration of sequielize

--- back-nest/src/app.module.ts
+++ back-nest/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'kayo',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
