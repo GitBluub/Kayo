201f9f067b2355eeb88db85b20e28e92da4a0876
arthur.jamet <arthur.jamet@epitech.eu>
2021-11-11 17:14:14 +0100

    routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+import Login from './Login';
 
-function App() {
 
-  return <Container>
-    <Box sx={{marginTop: 8,display: 'flex',flexDirection: 'column',alignItems: 'center',}}>
-      <TextField margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
-      <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password"/>
-      <Button variant="contained">Log In</Button>
-    </Box>
-  </Container>;
-}
+const App = () => (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+      </Routes>
+    </Router>
+)
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
---
+++ front/src/Login.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+function Login() {
+
+  return <Container>
+    <Box sx={{marginTop: 8,display: 'flex',flexDirection: 'column',alignItems: 'center',}}>
+      <TextField margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
+      <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password"/>
+      <Button variant="contained">Log In</Button>
+    </Box>
+  </Container>;
+}
+
+export default Login;
