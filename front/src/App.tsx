e5e69b76f4701a492c11237256549e1e51ab5646
arthur.jamet <arthur.jamet@epitech.eu>
2021-11-11 16:47:29 +0100

    basic static login form

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './App.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
-interface AppProps {}
+function App() {
 
-function App({}: AppProps) {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <Container>
+    <Box sx={{marginTop: 8,display: 'flex',flexDirection: 'column',alignItems: 'center',}}>
+      <TextField margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus />
+      <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password"/>
+      <Button variant="contained">Log In</Button>
+    </Box>
+  </Container>;
 }
+
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 export default App;
