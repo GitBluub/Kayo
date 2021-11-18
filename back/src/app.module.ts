7addb40f993fdbbb7b66b3e6acf786cde2d521d3
Bluub <louis.auzuret@gmail.com>
2021-11-18 14:35:08 +0100

    nest generate module, service, controller fo rsubscription and widget

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,6 +6,8 @@ import { User } from './user/models/user.model';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { WidgetModule } from './widget/widget.module';
 import authConfig from './config/authConfig';
 
 @Module({
@@ -28,6 +30,8 @@ import authConfig from './config/authConfig';
     }),
     UserModule,
     AuthModule,
+    SubscriptionModule,
+    WidgetModule,
   ],
   controllers: [AppController],
   providers: [AppService],
