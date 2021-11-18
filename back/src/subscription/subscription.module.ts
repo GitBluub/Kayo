c95e8e0a57a04a75a135c5c38eb840f2cccf800c
Bluub <louis.auzuret@gmail.com>
2021-11-18 15:10:52 +0100

    added sequelize module to subscription module

--- back/src/subscription/subscription.module.ts
+++ back/src/subscription/subscription.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Subscription } from 'rxjs';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Subscription])],
   controllers: [SubscriptionController],
-  providers: [SubscriptionService]
+  providers: [SubscriptionService],
+  exports: [SequelizeModule, SubscriptionService],
 })
 export class SubscriptionModule {}
