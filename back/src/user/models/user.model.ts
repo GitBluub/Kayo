2045bd5fc7191b92609ff7af2f5af4a19a421ebe
Bluub <louis.auzuret@gmail.com>
2021-11-18 15:07:07 +0100

    update user model

--- back/src/user/models/user.model.ts
+++ back/src/user/models/user.model.ts
@@ -2,13 +2,10 @@ import { Column, Model, Table } from 'sequelize-typescript';
 
 @Table({ tableName: 'users' })
 export class User extends Model {
-
-  @Column({ primaryKey: true, autoIncrement: true, allowNull: false })
-  id: number;
-
   @Column({unique: true, allowNull: false})
   name: string;
 
   @Column
   password: string;
+
 }
