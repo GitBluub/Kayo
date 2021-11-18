8ad0b8ad4f7850a9ad10fbeed2a1e2906792e263
Bluub <louis.auzuret@gmail.com>
2021-11-18 15:11:05 +0100

    added sequelize module to widget module

--- back/src/widget/widget.module.ts
+++ back/src/widget/widget.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WidgetController } from './widget.controller';
 import { WidgetService } from './widget.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Widget } from './models/widget.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Widget])],
   controllers: [WidgetController],
-  providers: [WidgetService]
+  providers: [WidgetService],
+  exports: [SequelizeModule, WidgetService],
 })
 export class WidgetModule {}
