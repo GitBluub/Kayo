c8a45af5cddd9a3dc98dc514bbe169b53fdb92f6
Bluub <louis.auzuret@gmail.com>
2021-11-19 10:35:59 +0100

    widget model belongsTo user

--- back/src/widget/models/widget.model.ts
+++ back/src/widget/models/widget.model.ts
@@ -1,15 +1,31 @@
-import { BelongsTo, Column, Model, Table } from 'sequelize-typescript';
+import { BelongsTo, Column, ForeignKey, Model, Table } from 'sequelize-typescript';
 import { User } from "../../user/models/user.model"
 
 @Table({ tableName: 'widgets' })
 export class Widget extends Model {
-  	@Column({allowNull: false})
-  	name: string;
+	@Column({allowNull: false})
+	name: string;
 
-	@Column
-	params: Array<number | string>
+	@ForeignKey(() => User)
+	userId: number;
 
 	@BelongsTo(() => User)
-	@Column
 	user: User
+}
+@Table({ tableName: 'parameters'})
+export class Parameter extends Model {
+	@Column
+	name: string
+
+	@Column
+	type: string;
+
+	@Column
+	value: string;
+
+	@ForeignKey(() => Widget)
+	widgetId: number;
+
+	@BelongsTo(() => Widget)
+	widget: Widget
 }
--- back/src/widget/widget.module.ts
+++ back/src/widget/widget.module.ts
@@ -3,11 +3,14 @@ import { WidgetController } from './widget.controller';
 import { WidgetService } from './widget.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Widget } from './models/widget.model';
+import { User } from "../user/models/user.model";
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Widget])],
+  imports: [UserModule, SequelizeModule.forFeature([Widget])],
   controllers: [WidgetController],
   providers: [WidgetService],
   exports: [SequelizeModule, WidgetService],
 })
 export class WidgetModule {}
+
