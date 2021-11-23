ea693633a0280b3f96b86cbb8c8ec18c019dffd4
Bluub <louis.auzuret@gmail.com>
2021-11-23 10:50:25 +0100

    subscription service create with dto

--- back/src/subscription/subscription.service.ts
+++ back/src/subscription/subscription.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Subscription } from 'rxjs';
 
 @Injectable()
-export class SubscriptionService {}
+export class SubscriptionService {
+	constructor(
+		@InjectModel(Subscription)
+		private subscriptionModel: typeof Subscription,
+	) {}
+
+	//async create(subscriptionDto: SubscriptionDto): Promise<Subscription> {
+	//	return this.subscriptionModel.create(subscription);
+	//}
+}
