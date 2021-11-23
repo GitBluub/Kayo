cba1d9cc5b6f50da939cb7b7919a93fa96499bfa
arthur.jamet <arthur.jamet@epitech.eu>
2021-11-23 20:56:54 +0100

    service page: put service card in simple widgets

--- front/src/Scenes/Services.tsx
+++ front/src/Scenes/Services.tsx
@@ -5,23 +5,17 @@ import ParameterCard from '../Components/ParameterCard/ParameterCard';
 import ParameterCardGroup from '../Components/ParameterCard/ParameterCardGroup';
 import ParameterCardButton from '../Components/ParameterCard/ParameterCardButton';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ServiceCard from '../Components/ServiceCard';
 
 const Services = () => {
 	return <Grid container alignItems="center" justifyContent="center" direction="column">
 			<ParameterCardTitle>Available Services</ParameterCardTitle>
 			<ParameterCardGroup title="Connected Services">
-				<ParameterCard name="Spotify">
-					<ParameterCardButton><DeleteOutlineIcon sx={{ color: "red", fontSize: 35}}/></ParameterCardButton>
-				</ParameterCard>
-				<ParameterCard name="Weather"/>
+				<ServiceCard serviceName="Spotify" actionType="delete" action={() => {}}/>
+				<ServiceCard serviceName="Weather" actionType="none" action={() => {}}/>
 			</ParameterCardGroup>
 			<ParameterCardGroup title="Other Services">
-				<ParameterCard name="Spotify">
-				<ParameterCardButton>
-					<AddCircleIcon sx={{ color: "green", fontSize: 35}}/>
-				</ParameterCardButton>
-				</ParameterCard>
+				<ServiceCard serviceName="Spotify" actionType="add" action={() => {}}/>
 			</ParameterCardGroup>
 		</Grid>
 }
