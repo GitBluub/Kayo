b9aca6f5c9caf74033c88cd035c1998542c74c42
arthur.jamet <arthur.jamet@epitech.eu>
2021-11-25 18:07:48 +0100

    remove useless param

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from "./Scenes/Home";
 
 
-const App = (props: any) => {
+const App = () => {
 	return <ThemeProvider theme={{}}>
 		<Grid container alignItems="center" justifyContent="center" sx={{marginTop: 8}}>
     	<Card style={{ borderRadius: 8, width: "80%"}} variant="outlined" sx={{paddingBottom:3, display: 'flex',flexDirection: 'column',alignItems: 'center', boxShadow: 20}}>
