217229a983d08201a73383f3c5b6411b2c26aa3f
Bluub <louis.auzuret@gmail.com>
2021-12-01 17:31:59 +0100

    stocks module

---
+++ back/src/stocks/stocks.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { Parameter } from 'src/widget/models/parameter.model';
+
+@Injectable()
+export class StocksService {
+
+	async getData(widgetName: string, params: Parameter[]) {
+
+	}
+}
