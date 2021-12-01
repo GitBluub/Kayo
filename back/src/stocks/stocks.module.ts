66edf61eac03a01596a0ed4055d092073f4906d2
Bluub <louis.auzuret@gmail.com>
2021-12-01 17:44:30 +0100

    stocks module

--- back/src/stocks/stocks.module.ts
+++ back/src/stocks/stocks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { StocksService } from './stocks.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [StocksService],
   exports: [StocksService]
 })
--- back/src/stocks/stocks.service.ts
+++ back/src/stocks/stocks.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios, { AxiosInstance } from 'axios';
 import { Parameter } from 'src/widget/models/parameter.model';
 
 @Injectable()
 export class StocksService {
+	axiosInstance: AxiosInstance
+	apiKey: string
+	constructor(
+		private configService: ConfigService
+	) {
+		this.apiKey = this.configService.get("STOCKS_API_KEY")
+		this.axiosInstance = axios.create({
+			baseURL: "https://www.alphavantage.co/query",
+		});
+	}
 
 	async getData(widgetName: string, params: Parameter[], token: string) {
-
+		const symbol = params[0].value;
+		const res = await this.axiosInstance.get(`?function=TIME_SERIES_INTRADAY&\
+		symbol=${params[0].value}&interval=5min&apikey=${this.apiKey}`)
+		const quotes = res.data["Time Series (5min)"]
+		const keys = quotes.keys();
+		return {
+			symbol,
+			total: quotes[keys[0]]['2, high'],
+			open: quotes[keys.slice(-1)]['2, high']
+		}
 	}
 }
