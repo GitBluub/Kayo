fae95169cf0fa4288d108a206425010857e65bf4
Bluub <louis.auzuret@gmail.com>
2021-12-01 17:59:58 +0100

    weather module

--- back/src/weather/weather.module.ts
+++ back/src/weather/weather.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { WeatherService } from './weather.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [WeatherService],
   exports: [WeatherService]
 })
