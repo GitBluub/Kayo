aba77ffa70dd63ffbdec328d5c0290f39d3e0d5d
Bluub <louis.auzuret@gmail.com>
2021-12-02 15:39:18 +0100

    front update

--- front/src/Views/Components/Widgets/Spotify/FavoriteArtistWidget.tsx
+++ front/src/Views/Components/Widgets/Spotify/FavoriteArtistWidget.tsx
@@ -3,11 +3,11 @@ import { SpotifyWidget, BestType } from './SpotifyWidget';
 
 interface FavoriteArtistWidgetInterface {
 	artistName: string,
-	illustration: string,
+	illustrationUrl: string,
 }
 
 const FavoriteArtistWidget = (props: FavoriteArtistWidgetInterface) => (
-	<SpotifyWidget leftTitle="Favorite artist" title={props.artistName} illustration={props.illustration} subtitle=""/>
+	<SpotifyWidget leftTitle="Favorite artist" title={props.artistName} illustration={props.illustrationUrl} subtitle=""/>
 )
 
 export { FavoriteArtistWidget, FavoriteArtistWidgetInterface }
