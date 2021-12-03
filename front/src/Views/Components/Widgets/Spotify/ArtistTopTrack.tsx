c1d5dbf3969ca72f10819cfa2bfe3273e7704ac7
Arthi-chaud <arthur.jamet@epitech.eu>
2021-12-03 17:36:58 +0100

    spotify artist top track: adding album name

--- front/src/Views/Components/Widgets/Spotify/ArtistTopTrack.tsx
+++ front/src/Views/Components/Widgets/Spotify/ArtistTopTrack.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
+import type { SpotifyWidgetData } from 'src/Controllers/WidgetFactories/SpotifyWidgetFactory';
 import { SpotifyWidget, BestType } from './SpotifyWidget';
 
-interface ArtistTopTrackInterface {
-	artistName: string,
-	track: string
-	illustration: string,
-}
 
-const ArtistTopTrack = (props: ArtistTopTrackInterface) => (
-	<SpotifyWidget leftTitle={props.artistName + "'s top track"} title={props.track} illustration={props.illustration} subtitle=""/>
+const ArtistTopTrack = (props: SpotifyWidgetData) => (
+	<SpotifyWidget leftTitle={props.artistName + "'s top track"} title={`'${props.trackName as string}' from ${props.albumName as string}`} illustration={props.illustrationUrl} subtitle=""/>
 )
 
-export { ArtistTopTrack, ArtistTopTrackInterface }
+export { ArtistTopTrack }
