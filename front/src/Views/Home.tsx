585469233f9e8468ed382c72ebf16ceb16006617
Bluub <louis.auzuret@gmail.com>
2021-12-03 14:33:44 +0100

    remove type warnings from class state

--- front/src/Views/Home.tsx
+++ front/src/Views/Home.tsx
@@ -56,12 +56,12 @@ export default class Home extends React.Component {
 				<Title>KAYO</Title>
 				<Grid container alignItems="center" justifyContent="center" direction="column" style={{ paddingTop: 30 }}>
 					{
-						this.state.widgetGroups.length == 0 ?
+						(this.state as HomeState).widgetGroups.length == 0 ?
 							<><Subtitle >No new widget, please consider one of the following options:</Subtitle>
 								<Subtitle><Link to="/widgets/add">Add a widget</Link></Subtitle>
 								<Subtitle><Link to="/services">Subscribe to a service</Link></Subtitle>
 							</>
-							: this.state.widgetGroups.map((group: WidgetGroupInterface) => <WidgetGroup key={group.serviceName} serviceName={group.serviceName} widgets={group.widgets} />)
+							: (this.state as HomeState).widgetGroups.map((group: WidgetGroupInterface) => <WidgetGroup key={group.serviceName} serviceName={group.serviceName} widgets={group.widgets} />)
 					}
 				</Grid>
 			</Grid>
